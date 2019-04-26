b9782f19fcaa4b7e3a8ba686c433ddb1ba126920
Aleksandr Chepurko <alexandrch9@gmail.com>
2019-04-27 02:30:24 +0300

    Commit to the new branch created from the bash

--- HelloTeam.java
+++ HelloTeam.java
@@ -2,8 +2,7 @@ public class HelloTeam {
 
     public static void main(String[] args) {
         System.out.println("Hello Team Uraaa!");
-        System.out.println("Uraaa!");
-        System.out.println("Changes for new branch");
+        System.out.println("Changes for new branch created from bash");
     }
 
 }
