5157be73785792be84e0efb23e4bde19d310e21e
Aleksandr Chepurko <alexandrch9@gmail.com>
2019-04-27 01:29:43 +0300

    The file HelloTeam.java with changes for new branch

--- HelloTeam.java
+++ HelloTeam.java
@@ -3,7 +3,7 @@ public class HelloTeam {
     public static void main(String[] args) {
         System.out.println("Hello Team Uraaa!");
         System.out.println("Uraaa!");
-        System.out.println("And one more time!");
+        System.out.println("Changes for new branch");
     }
 
 }
