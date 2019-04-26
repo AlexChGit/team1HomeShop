df36782b4b25bdd1f04f6a6cdb0be0c3d56e720d
Aleksandr Chepurko <alexandrch9@gmail.com>
2019-04-27 01:17:23 +0300

    One more time! Without get status ;)

--- HelloTeam.java
+++ HelloTeam.java
@@ -3,6 +3,7 @@ public class HelloTeam {
     public static void main(String[] args) {
         System.out.println("Hello Team Uraaa!");
         System.out.println("Uraaa!");
+        System.out.println("And one more time!");
     }
 
 }
